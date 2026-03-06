dd03c5d1fb8cd3c3a65baf601a218e01f2530a06
Maria Angelica Muñoz Lopez <marriangelica135@gmail.com>
2026-03-06 07:02:56 -0500

    Segunda version del motor
    Se le agrego una variable para empezar un día de la semana aleatorio y seguir el consecutivo.

--- motor.py
+++ motor.py
@@ -1,37 +1,47 @@
-import feature/inicio
+import inicio
 import Eventos
+import random
 #Motor principal
+
+#===============================================================================
+#                              MOTOR PRINCIPAL
+#===============================================================================
+
+day = 1
+days_weeks = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+start_week = random.randint(0,6)
 
 while day <= 10 and health > 0:
 
-    print("====================================================")
-    print("                THE LAST DAY -", day,"              ")
-    print("====================================================")
+    current_day = days_weeks[(start_week + day) % 7]
+
+    print("=======================================================")
+    print("             THE LAST DAY -", current_day, day,"       ")
+    print("=======================================================")
 
     print("\n               CURRENT STATUS                  \n")
-    print("Water:", water, "| Food:", food, "Energy:", energy)
-    print("Healt:", health, "| Residents:", population)
+    print("Healt:", health, "| Food:", food, "| Water:", water, "Energy:", energy, "| Population:", population)
 
-
-    food, water, energy, health = eventos_random(difficult_level, food, water, energy, health)
-
-
-    print("\n               NEW CURRENT STATUS                  ")
-    print("Water:", water, "| Food:", food, "Energy:", energy)
-    print("Healt:", health, "| Residents:", population)
-
+#---------------------------------------------------------------------------------------------------------------
+#  Esto aplica los eventos en los transcursos del dia
+    
+    food, water, energy, health = eventos_random(food, water, energy, health)
+#---------------------------------------------------------------------------------------------------------------
     day += 1
-    input("\nJust press Enter or a letter or number to continue...\n")
 
-if day == 11 and health and water and food and energy and population > 0:
-    print("===================================================")
-    print("    Congratulations, you survived until the 10th.  ")
-    print("===================================================")
+    print("\n               NEW CURRENT STATUS                  \n")
+    print("Healt:", health, "| Food:", food, "| Water:", water, "Energy:", energy, "| Population:", population)
 
-elif day <= 11 or health or water or food or energy or population
-    print("===================================================")
-    print("                     GAME OVER                     ")
+    
+    input("Just press Enter or a letter or number to continue...\n")
 
-    print("===================================================")
+if day == 11 and health and population > 0:
+    print("======================================================")
+    print("    Congratulations, you survived until the 10th.     ")
+    print("======================================================")
 
+else:
+    print("======================================================")
+    print("                     GAME OVER                        ")
+    print("======================================================")
 
